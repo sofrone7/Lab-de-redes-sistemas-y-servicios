2e3092ce7e60a647d27744ee593c61f8ccd5b9cc
sofrone7 <silviusofrone7@gmail.com>
2021-03-04 10:39:35 +0000

    ping se conecta al puerto indicado y se cierra la conexión

--- p1.1/py_TCP_ping/ping_oc.py
+++ p1.1/py_TCP_ping/ping_oc.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
 
 if len(sys.argv) < 2 or len(sys.argv) > 3:
-  print('Usage: <Server IP> [<PING Port>]')
+  print('Usage:', sys.argv[0], '<Server IP> [<PING Port>]')
+
+#Definición de variables
+servIP = sys.argv[1]
+pingString = "s"
+pingServPort = sys.argv[2]
+
+#Crear socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Conexión
+sock.connect((servIP, int(pingServPort)))
+
+sock.close()
