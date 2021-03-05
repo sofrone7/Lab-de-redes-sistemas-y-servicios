ce781c9d7483658c8ad1cbaa13fa3b9afc65d179
sofrone7 <silviusofrone7@gmail.com>
2021-03-05 17:54:54 +0000

    tiempo transcurrido por ping

--- p1.1/py_TCP_ping/ping_oc.py
+++ p1.1/py_TCP_ping/ping_oc.py
@@ -4,6 +4,8 @@ import sys
 import socket
 import time
 
+clk_id1 = time.CLOCK_REALTIME # System-wide real-time clock
+
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print('Usage:', sys.argv[0], '<Server IP> [<PING Port>]')
 
@@ -24,7 +26,9 @@ try:
   while True:
     seq += 1
     pingString = b"mensaje" #convertido a bytes
-    #clock
+    #time_send = time.clock_gettime_ns(clk_id1) #tiempo envío ping en ns
+    #time_send = float(round(time.time() * 1000.0))
+    time_send = time.time() #tiempo envío ping en seg
     sock.send(pingString);
       
     totalBytesRcvd = 0;
@@ -32,7 +36,13 @@ try:
     bytesRcvd = 0
       
     if sock.recv(16): #comprobamos que sólo si recibimos respuesta de vuelta
-      print(pingStringLen, 'bytes from', servIP, 'icmp_seq=', seq, 'ttl= time= ms')
+      #time_recv = time.clock_gettime_ns(clk_id1)
+      #time_recv = float(round(time.time() * 1000.0))
+      time_recv = time.time()
+      tiempo = (time_recv - time_send) * 1000
+      print("{} bytes from {}: icmp_ser={} ttl= time={:.3f} ms".format(pingStringLen, servIP, seq, tiempo))
+      #print resultado.format(pingStringLen, servIP, seq, tiempo)
+      #print(pingStringLen, 'bytes from', servIP, 'icmp_seq=', seq, 'ttl= ' 'time={:.2f}', tiempo, 'ms')
     
     time.sleep(1)
 except KeyboardInterrupt:
