cb79905598414b66796ac898e1705ce8a5106341
sofrone7 <silviusofrone7@gmail.com>
2021-03-05 19:36:56 +0000

    ping tcp with python finished

--- p1.1/py_TCP_ping/ping_oc.py
+++ p1.1/py_TCP_ping/ping_oc.py
@@ -3,9 +3,25 @@
 import sys
 import socket
 import time
+import math
+
+def desvEstandar(array, avg, seq):
+  var = 0 
+  resultado = 0
+  for i in range(len(array)):
+    var += pow((array[i] - avg) ,2)
+  var = var/seq
+  resultado = math.sqrt(var)
+  #print('resultado', resultado)
+  return resultado
 
 clk_id1 = time.CLOCK_REALTIME # System-wide real-time clock
 ttl = 64
+recv = 0 #variable que contendrá el nº de pings recibidos
+array = []
+time_total = 0
+time_min = 0
+time_max = 0
 
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print('Usage:', sys.argv[0], '<Server IP> [<PING Port>]')
@@ -37,13 +53,32 @@ try:
     bytesRcvd = 0
       
     if sock.recv(16): #comprobamos sólo si recibimos respuesta de vuelta
+      recv += 1
       time_recv = time.time()
       tiempo = (time_recv - time_send) * 1000
-      print("{} bytes from {}: icmp_ser={} ttl= time={:.3f} ms".format(pingStringLen, servIP, seq, tiempo))
+      print("{} bytes from {}: icmp_ser={} ttl={} time={:.3f} ms".format(pingStringLen, servIP, seq, ttl, tiempo))
+      time_total += tiempo
+      array.append(tiempo) #fill array with the time data
+      #for i in range(len(array)):
+        #print( array[i])
+      if time_min == 0 or tiempo < time_min:
+        time_min = tiempo
+      
+      if time_max == 0 or tiempo > time_max:
+        time_max = tiempo
+        
+      avg = time_total/seq
+        
+    else:
+      recv -= 1
     
     time.sleep(1)
 except KeyboardInterrupt:
-  print('stop')
+  mdev = desvEstandar(array, avg, seq)
+  #print('mdev', mdev)
+  print()
+  print("--- {} ping statistics ---".format(servIP))
 finally:
-  print('close socket')
+  print('{} packets transmitted, {} received, {}% packet loss, time {:.3f} ms'.format(seq, recv, ((seq - recv)/seq) * 100.0, time_total))
+  print('rtt min/avg/max/mdev = {:.3f}/{:.3f}/{:.3f}/{:.3f} ms'.format(time_min, avg, time_max, mdev))
   sock.close()
