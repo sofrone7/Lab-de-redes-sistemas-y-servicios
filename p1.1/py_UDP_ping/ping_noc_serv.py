ced637d358e584c4ddb68f688b212caf1120fcbf
sofrone7 <silviusofrone7@gmail.com>
2021-03-10 22:37:01 +0000

    comentarios limpios

--- p1.1/py_UDP_ping/ping_noc_serv.py
+++ p1.1/py_UDP_ping/ping_noc_serv.py
@@ -16,19 +16,5 @@ while True:
   message, clntPingAddr = sock.recvfrom(16)
   print('Handling client', clntPingAddr)
   sock.sendto(message, clntPingAddr)
-  #try:
-    #print('conexion con', clntPingAddr)
-    
-    #while True:
-      #bytesRcvd = sock.recvfrom(16)
-      #print('received {!r}'.format(bytesRcvd))
-      #if bytesRcvd:
-        #print('sending data back to the client')
-        #sock.sendto(bytesRcvd)
-      #else:
-        #print('no hay datos recibidos de')
-        #break
-        
-  #finally:
-    #print('Finalizado')
+
     
