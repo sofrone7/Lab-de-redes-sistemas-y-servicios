20e2a5540101276d9e4d217331014041da8f8434
sofrone7 <silviusofrone7@gmail.com>
2021-03-20 11:33:04 +0000

    no cierra conexión con el socket de forma adecuada

--- p1.2/cliente-servidor/cliente.py
+++ p1.2/cliente-servidor/cliente.py
@@ -4,6 +4,16 @@
 import sys
 import socket
 import select
+from threading import Thread
+#import shlex
+
+def escucha():
+  while True:
+    datos = sock.recv(1024)
+    if datos:
+      print('\n< ',datos.decode("utf-8"))
+    if not datos:
+      sock.close()   
 
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print('Usage:', sys.argv[0], '<Server IP> [<PING Port>]')
@@ -18,11 +28,26 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Conexión
 sock.connect((servIP, int(pingServPort)))
 
+#socks = [sock]
+
+job = Thread(target=escucha)
+job.start()
+
 while True:
-  try:
-    mensaje = input('Chat: ')
-    print('Mensaje:', bytes(mensaje, 'utf-8'))
-    sock.sendall(bytes(mensaje, 'utf-8')) 
-  except sock.recv(1024):
-    datos = sock.recv(1024)
-    print(datos.decode("utf-8"))
+  #for s in socks:
+  mensaje = input('> ')
+  #mensaje += '\n'
+  #print('Mensaje:', mensaje)
+  sock.sendall(bytes(mensaje, 'utf-8')) # El mensaje debe ir codificado
+  #for s in socks:
+    #datos = sock.recv(1024)
+    #print(datos.decode("utf-8"))
+    #if not datos:
+      #sock.close()
+  #if datos:
+    #print(datos.decode("utf-8"))
+  #else:
+    #if not datos:
+      #mensaje = input('Chat: ')
+      #print('Mensaje:', mensaje)
+      #sock.sendall(bytes(mensaje, 'utf-8')) # El mensaje debe ir codificado
