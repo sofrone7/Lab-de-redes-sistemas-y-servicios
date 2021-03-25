2f428d44a82f8724b9f4a4992d1c5f05a809fe50
sofrone7 <silviusofrone7@gmail.com>
2021-03-25 12:55:22 +0000

    las direcciones las guardo en una lista

--- p1.2/p2p/servidor_usuarios.py
+++ p1.2/p2p/servidor_usuarios.py
@@ -17,29 +17,35 @@ ServSock.bind(('', int(ServPort)))
 ServSock.listen(10)
 
 usuarios = [ServSock]
+direcciones = []
 
 while usuarios:
-	ready_to_read, ready_to_write, error = select.select(usuarios,[] ,[] )
-	for s in ready_to_read:
-		if s is ServSock:
-			connection, clntAddr = ServSock.accept()
-			print( 'Nuevo usuario:', clntAddr)
-			for x in usuarios:
-				if x != ServSock:
-					msg = x.getpeername()
-					print(msg)
-					connection.send(pickle.dumps(msg))
+  ready_to_read, ready_to_write, error = select.select(usuarios,[] ,[] )
+  for s in ready_to_read:
+    if s is ServSock:
+      connection, clntAddr = ServSock.accept()
+      print( 'Nuevo usuario:', clntAddr)
+      usuarios.append(connection)
+      connection.sendall(pickle.dumps(direcciones))
+      direcciones.append(clntAddr[0])
+      direcciones.append(clntAddr[1])
+      
+			#for x in usuarios:
+				#if x != ServSock:
+					#msg = x.getpeername()
+					#print(msg)
+					#connection.send(pickle.dumps(msg))
 					#msg = pickle.dumps(msg)
 					#connection.sendall(bytes(msg, "utf-8"))
-			usuarios.append(connection)
-		else:
-			try:	
-				conn = ServSock.getpeername()
+			
+    else:
+      try:	
+        conn = ServSock.getpeername()
 				#print(conn)
-				s.connect(conn)
-			except:
-				print('Ya no hay conexión con: ', s.getpeername())
-				usuarios.remove(s)
-				s.close()
+        s.connect(conn)
+      except:
+        print('Ya no hay conexión con: ', s.getpeername())
+        usuarios.remove(s)
+        s.close()
 
 
