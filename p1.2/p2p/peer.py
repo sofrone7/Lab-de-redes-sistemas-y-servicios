01e3c384626c9a192379af53a144bb155132b756
sofrone7 <silviusofrone7@gmail.com>
2021-03-25 19:55:49 +0000

    no sé mandar puerto de escucha del peer

--- p1.2/p2p/peer.py
+++ p1.2/p2p/peer.py
@@ -13,12 +13,26 @@ ServPort = sys.argv[2]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((servIP, int(ServPort)))
 
+ServSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ServSock.setblocking(0)
+
+ServSock.bind(('', 0))
+ServSock.listen(10)
+print(ServSock.getsockname()[1])
+port = ServSock.getsockname()[1]
+sock.sendall(bytes(port, 'utf-8'))
+#sock.sendall(bytes(mensaje, 'utf-8'))
+
+direcciones = []
 try:
-	while True:
-		datos = sock.recv(1024)
-		if datos:
-			datos = pickle.loads(datos)
-			print(datos)
+  while True:
+    datos = sock.recv(1024)
+    if datos:
+      datos = pickle.loads(datos)
+      print(datos)
+      for x in datos:
+        print(x)
+			
 
 except KeyboardInterrupt:
   sock.close()
--- p1.2/p2p/servidor_usuarios.py
+++ p1.2/p2p/servidor_usuarios.py
@@ -19,33 +19,42 @@ ServSock.listen(10)
 usuarios = [ServSock]
 direcciones = []
 
-while usuarios:
-  ready_to_read, ready_to_write, error = select.select(usuarios,[] ,[] )
-  for s in ready_to_read:
-    if s is ServSock:
-      connection, clntAddr = ServSock.accept()
-      print( 'Nuevo usuario:', clntAddr)
-      usuarios.append(connection)
-      connection.sendall(pickle.dumps(direcciones))
-      direcciones.append(clntAddr[0])
-      direcciones.append(clntAddr[1])
-      
-			#for x in usuarios:
-				#if x != ServSock:
-					#msg = x.getpeername()
-					#print(msg)
-					#connection.send(pickle.dumps(msg))
-					#msg = pickle.dumps(msg)
-					#connection.sendall(bytes(msg, "utf-8"))
-			
-    else:
-      try:	
-        conn = ServSock.getpeername()
-				#print(conn)
-        s.connect(conn)
-      except:
-        print('Ya no hay conexión con: ', s.getpeername())
-        usuarios.remove(s)
-        s.close()
+try:
+  while usuarios:
+    ready_to_read, ready_to_write, error = select.select(usuarios,[] ,[] )
+    for s in ready_to_read:
+      if s is ServSock:
+        connection, clntAddr = ServSock.accept()
+        port = connection.recv(1024)
+        print(port.decode("utf-8"))
+        print(port)
+        print( 'Nuevo usuario:', clntAddr)
+        print(connection.getpeername())
+        usuarios.append(connection)
+        connection.sendall(pickle.dumps(direcciones))
+        direcciones.append(clntAddr)
+        #direcciones.append(clntAddr[1])
+        
+  			#for x in usuarios:
+  				#if x != ServSock:
+  					#msg = x.getpeername()
+  					#print(msg)
+  					#connection.send(pickle.dumps(msg))
+  					#msg = pickle.dumps(msg)
+  					#connection.sendall(bytes(msg, "utf-8"))
+  			
+      else:
+        try:	
+          conn = ServSock.getpeername()
+  				#print(conn)
+          s.connect(conn)
+        except:
+          print('Ya no hay conexión con: ', s.getpeername())
+          usuarios.remove(s)
+          s.close()
+except KeyboardInterrupt:
+  for s in usuarios:
+    if s != ServSock:
+      s.close()
 
 
