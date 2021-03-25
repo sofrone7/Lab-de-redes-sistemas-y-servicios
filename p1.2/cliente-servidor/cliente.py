a06a6f812465218c6a4b53056fc3c28a06bb4551
sofrone7 <silviusofrone7@gmail.com>
2021-03-25 12:22:39 +0000

    cliente con select() y manda ficheros

--- p1.2/cliente-servidor/cliente.py
+++ p1.2/cliente-servidor/cliente.py
@@ -4,25 +4,7 @@
 import sys
 import socket
 import select
-#import threading
-from threading import Thread # Para no tener que poner threading.Thread
-#import shlex
-
-def escucha():
-  while True:
-    datos = sock.recv(1024)
-    if datos:
-      print('\n< ',datos.decode("utf-8"))
-    if not datos:
-      sock.close()
-      print('Conexión cerrada')
-      
-#def salida():
-  #while True:
-    #mensaje = input('> ')
-    #mensaje += '\n'
-    #print('Mensaje:', mensaje)
-    #sock.sendall(bytes(mensaje, 'utf-8')) # El mensaje debe ir codificado
+import re #expresiones regulares
 
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print('Usage:', sys.argv[0], '<Server IP> [<PING Port>]')
@@ -37,17 +19,34 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Conexión
 sock.connect((servIP, int(pingServPort)))
 
-#socks = [sock]
+lectura = [sock, sys.stdin]
 
-#back = Thread(target=salida)
-job = Thread(target=escucha)
-
-job.start()
-#back.start()
 try:
-  while True:
-    mensaje = input('> ')
-    sock.sendall(bytes(mensaje, 'utf-8')) # El mensaje debe ir codificado
+  while lectura:
+    read, write, error = select.select(lectura, [], [])
+    for s in read:
+      if s is sock:
+        datos = sock.recv(1024)
+        if datos:
+          print('\n< ',datos.decode("utf-8"))
+        if not datos:
+          sock.close()
+          print('Conexión cerrada')
+      if s is sys.stdin:
+        mensaje = input('> ')
+        if re.findall('[.]txt$', mensaje):
+          print('ok')
+          try:
+            f = open(mensaje, 'r')
+            bytes_f = f.read().encode()
+            sock.sendall(bytes_f)
+          except IOError:
+            print('El fichero no es accesible')
+            sock.sendall(bytes(mensaje, 'utf-8'))
+          finally:
+            f.close()
+        else:
+          sock.sendall(bytes(mensaje, 'utf-8')) # El mensaje debe ir codificado
 except KeyboardInterrupt:
   sock.close()
   
