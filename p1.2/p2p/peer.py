08529c6ee889ff92ed34c4131114dfc109c5f776
sofrone7 <silviusofrone7@gmail.com>
2021-03-28 09:59:15 +0000

    el chat funciona, falta desconexión

--- p1.2/p2p/peer.py
+++ p1.2/p2p/peer.py
@@ -31,14 +31,22 @@ datos = ServSock.recv(1024)
 if datos:
   lista = pickle.loads(datos)
   #print(lista)
-  for x in lista:
-    print('Conectamos con:', x)
-    sockconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sockconn.connect(x)
-    lectura.append(sockconn)
+  if lista: #Si la lista no está vacía
+    for x in lista:
+      print('Conectamos con:', x)
+      #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #s.connect(x)
+        #lectura.append(s)
+        #print('Socket de', x, 'es:', s)
+        #print()
+      soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      soc.connect(x)
+      lectura.append(soc)
 print('Lista:')
 for p in lectura:
-  if p != sock and p != sys.stdin:
+  if p != sys.stdin:
+    #conn = sock.getpeername()
+    #print(conn)
     print(p)
 print()
 
@@ -53,8 +61,13 @@ try:
         print('Conexión con peer:', clntAddr)
         lectura.append(connection)
         for x in lectura:
-          if x != sys.stdin and x != sock:
+          if x != sys.stdin:
+            #print(x.getpeername())
+            #print(x)
+            #conn = sock.getpeername()
+            #print(conn)
             print(x)
+        print()
       if s is sys.stdin:
         mensaje = input('> ')
         if mensaje:
@@ -64,9 +77,11 @@ try:
               #sockconn.sendall(bytes(mensaje, 'utf-8')) 
       #if s is sock:
       else:
-        datos = s.recv(1024)
-        if datos:
-          print('\n< ',datos.decode("utf-8"))
+        if s != sock:
+          #print(s)
+          datos = s.recv(1024)
+          if datos:
+            print('\n< ',datos.decode("utf-8"))
 			
 
 except KeyboardInterrupt:
