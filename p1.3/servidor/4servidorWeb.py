6cc09fefa79d50eec5b4c900596056139d8f947d
Silviu <silviusofrone@gmail.com>
2021-04-28 21:01:17 +0200

    ambos modos y timeout

--- p1.3/servidor/4servidorWeb.py
+++ p1.3/servidor/4servidorWeb.py
@@ -6,6 +6,12 @@ import socket
 import select
 import re #expresiones regulares
 import os
+import signal
+import time
+
+# Manejador de la señal SIGALARM
+def timer(signum, stack):
+	print('Servidor web inactivo')
 
 if len(sys.argv) != 2:
 	print('Usage:', sys.argv[0], '<Server Port>\n')
@@ -29,6 +35,11 @@ entrantes = [ServSock]
 # Sockets que van a ready_to_write
 salientes = []
 
+# Establecemos un manejador para la señal SIGALRM. La función que maneja la señal es timer
+signal.signal(signal.SIGALRM, timer)
+
+# Cuando pasen 10 segundos se solicita que SIGALARM envié una señal a su manejador
+signal.alarm(10) # Inicializamos la alarma aquí, ya que el servidor web puede estar corriendo pero no recibir ninguna petición 
 #try:
 while entrantes:
 	ready_to_read, ready_to_write, error = select.select(entrantes, salientes, entrantes)
@@ -36,18 +47,22 @@ while entrantes:
 		if s is ServSock: #Si s es el socket del servidor aceptamos las conexiones de los clientes que entran al chat
 			connection, clntAddr = ServSock.accept()
 			print( 'Conexión con:', clntAddr)
+			print()
 			entrantes.append(connection) # Añadimos a la lista de entrantes el nuevo cliente
 		else:
 			recvdata = s.recv(5000).decode()
 			if recvdata:
+				signal.alarm(0) #En caso de recibir datos (una petición) entendemos que el servidro ya no va a estar inactivo (tiene trabajo que hacer) -> quitamos la alarma para que no siga corriendo desde donde se ha quedado
 				print(recvdata)
 				pieces = recvdata.split()
 
 				metodo = pieces[0]
 				solicitud = pieces[1]
+				version = pieces[2]
 
 				print()
-				print('Solicitud:', solicitud)
+				print('Solicitud de ',s.getpeername())
+				#print('Solicitud:', solicitud)
 
 				solicitud = solicitud.lstrip('/') #Elimina el / inicial
 				if(solicitud == ''):
@@ -72,15 +87,20 @@ while entrantes:
 					f.close()
 
 					tam = os.stat(solicitud).st_size
-					data = 'HTTP/1.1 200 OK\r\n'
+					data =  version + ' 200 OK\r\n'
 					data += 'Content-type: ' + str(tipo) + '\r\n'
-					data += 'Content-length: ' + str(tam) + '\r\n'
+					data += 'Content-length: ' + str(tam) + '\r\n' #Importante la longitud ya que el navegador debe saber cuando has acabado de mandar información, sino no se podrá implementar el modo persistente
 					data += '\r\n'
 				except FileNotFoundError: 
 					f = open('error_404.html', 'rb') 
 					bytes_f = f.read() 
 					f.close()
-					data = 'HTTP/1.1 404 Not Found\r\n'
+
+					tam = os.stat('error_404.html').st_size
+					data = version + ' 404 Not Found\r\n'
+					data += 'Content-type: text/html\r\n'
+					data += 'Content-length: ' + str(tam) + '\r\n'
+					data += '\r\n'
 
 				print()
 				print(data)
@@ -88,13 +108,17 @@ while entrantes:
 				final_data = data.encode()
 				final_data += bytes_f		
 				s.sendall(final_data)
-				#entrantes.remove(s)
-				#s.close()
 
+				if(version == 'HTTP/1.0'):
+					print('Modo no persistente, cierro conexión\n')
+					entrantes.remove(s)
+					s.close()
+				signal.alarm(10) #Una vez terminao su trabajo volvemos a establecer la alarma a 10 segundos (la reiniciamos ya que antes la pusimos a 0)
 			else: 
-				print('Ya no hay conexión con:',s.getpeername())
-				entrantes.remove(s) #En caso de perderse la conexión con algún cliente se le expulsa de la lista
-				s.close() #Y se cierra su socket asociado
+				if s != ServSock:
+					print('Ya no hay conexión con:',s.getpeername())
+					entrantes.remove(s) #En caso de perderse la conexión con algún cliente se le expulsa de la lista
+					s.close() #Y se cierra su socket asociado
 #except KeyboardInterrupt:
 	#ServSock.close() 
 	#print('Cerrado')
