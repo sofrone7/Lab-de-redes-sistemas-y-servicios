3b565a1f3338f10b9d4366ce2953d21846b933a4
sofrone7 <silviusofrone7@gmail.com>
2021-05-03 12:06:54 +0200

    utilizo un archivo de texto como intermediario en la peticion post

--- p1.3/servidor/4servidorWeb.py
+++ p1.3/servidor/4servidorWeb.py
@@ -9,10 +9,28 @@ import os
 import signal
 import time
 import requests
+import pathlib
 
 # Manejador de la señal SIGALARM
 def timer(signum, stack):
 	print('Servidor web inactivo')
+
+def functipo(recvdata, solicitud):
+	if 'text/html' in recvdata:
+		tipo = 'text/html'
+		if re.findall('[.]html$', solicitud): 
+			print('Solicitud:', solicitud)
+			return tipo
+		if re.findall('[.]txt$', solicitud):
+			print('Solicitud', solicitud)
+			return tipo
+		else:
+			solicitud += '.html'
+			print('SolicitudB:', solicitud)
+			return tipo
+
+#def remove_suffix(input_string, suffix):
+	#if suffix and input_string.endswith(
 
 if len(sys.argv) != 3:
 	print('Usage:', sys.argv[0], '<Modo> <Server Port>\n')
@@ -87,15 +105,19 @@ while entrantes:
 				if metodo=='GET':
 					if(solicitud == ''):
 						solicitud = 'index.html' #archivo index.html como default
-						
-					if 'text/html' in recvdata:
+					#tipo = functipo(recvdata, solicitud)
+					
+					if re.findall('[.]txt$', solicitud):
+						tipo = 'plain/text'
+						print('Solicitud', solicitud)
+					else:
 						tipo = 'text/html'
 						if re.findall('[.]html$', solicitud): 
 							print('Solicitud:', solicitud)
-						#else:
-							#solicitud += '.html'
-							#print('Solicitud:', solicitud)
-
+						else:
+							solicitud += '.html'
+							print('SolicitudB:', solicitud)
+				
 					if '.jpg' in recvdata:
 						tipo = 'image/jpg'
 						print('Solicitud:', solicitud)
@@ -103,6 +125,13 @@ while entrantes:
 						#tipo = 'image/webp'
 					if 'application/json' in recvdata:
 						tipo = 'application/json'
+					
+					if '.txt' in recvdata:
+						tipo = 'text/plain'
+						if re.findall('[.]html$', solicitud):
+							#solicitud = pathlib.Path(solicitud).with_suffix('.html')
+							print('Solicitud:', solicitud)
+
 
 					try: #Por tanto, intentamos abrirlo y enviar su infromación contenida
 						f = open(solicitud, 'rb') 
@@ -134,40 +163,63 @@ while entrantes:
 					s.sendall(final_data)
 				
 				if 'POST' in recvdata:
+					if 'application/x-www-form-urlencoded' in recvdata:
+						tipo = 'text/html'
+						if re.findall('[.]html$', solicitud): 
+							print('Solicitud:', solicitud)
+						#else:
+							#solicitud += '.html'
+							#print('Solicitud:', solicitud)
+
 					if '.php' in recvdata:
 						#tipo = 'application/x-www-form-urlencoded'
 						tipo = 'text/html'
 						if re.findall('[.]php$', solicitud): 
 							print('Solicitud:', solicitud)
 
+					#if '.txt' in recvdata:
+						#tipo = 'text/plain'
+						#if re.findall('[.]html$', solicitud):
+							#solicitud = solicitud.lstrip('.html')
+							#print('Solicitud:', solicitud)
+
+					datapost = 'Datos: '
+					for i in range (0, len(pieces)):
+						if i == (len(pieces) - 1):
+							print('Datos POST:' + str(pieces[i]) + '\n')
+							postdata = str(pieces[i])
+							postdata = postdata.split('&')
+							#print('Separado &:', postdata)
+							for i in range (0, len(postdata)):
+								datapost += postdata[i] + '\n'
+
+					try:
+						f = open('datos.txt', 'w')
+						f.write(datapost)
+					except IOError:
+						print('Fichero no accesible\n')
+					finally:
+						f.close()
+
 					try: #Por tanto, intentamos abrirlo y enviar su infromación contenida
 						f = open(solicitud, 'rb') 
 						bytes_f = f.read() #.encode() #Leemos el archivo y codificamos su información para ser enviada
 						f.close()
 						
-						datapost = 'Datos: '
-						for i in range (0, len(pieces)):
-							if i == (len(pieces) - 1):
-								print('Datos POST:' + str(pieces[i]) + '\n')
-								postdata = str(pieces[i])
-								postdata = postdata.split('&')
-								#print('Separado &:', postdata)
-								for i in range (0, len(postdata)):
-									datapost += postdata[i] + '\n'
-
 						tam = os.stat(solicitud).st_size
+						#tam = 5000
 						#response = '<html><body><center><h3>Datos del formulario</h3><p> {} </p></center></body></html>'.format(datapost).encode()
-
-						#tam = os.stat(response).st_size
+						#response = datapost.encode()
+						#tam += os.stat(response).st_size
 						data =  version + ' 200 OK\r\n'
 						#data = version + ' 307 Temporary Redirect\r\n'
 						#data += 'Location: welcome.php\r\n'
-						data += 'Content-encodig: gzip\r\n'
+						#data += 'Content-encodig: gzip\r\n'
 						data += 'Content-type: ' + str(tipo) + '; charset=UTF-8\r\n'
 						#data += 'Content-type: ' + str(tipo) + '\r\n'
 
 						data += 'Content-length: ' + str(tam) + '\r\n' #Importante la longitud ya que el navegador debe saber cuando has acabado de mandar información, sino no se podrá implementar el modo persistente	
-						data += 'vary: Accept-Encoding\r\n'
+						#data += 'vary: Accept-Encoding\r\n'
 						data += '\r\n'
 				
 					except FileNotFoundError: 
@@ -187,9 +239,9 @@ while entrantes:
 					print(data)
 
 					final_data = data.encode()
-					#final_data += response
 					#final_data += '<html><body>Welcome <?php echo $_POST["name"]; ?><br>Your email address is: <?php echo $_POST["email"]; ?></body></html>'.encode()
 					#final_data += postdata.encode()
+					#final_data += response
 					final_data += bytes_f	
 					#userdata = {"name":"pepe","email":"oliet"}
 					#resp = requests.post('http://localhost/8082/welcome.php', data = userdata)
